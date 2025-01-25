d5b2ded875980ca74158d5165df6aebdb565409c
drwuro <email@drwuro.com>
2025-01-25 21:33:14 +0100

    reuise client discover socket

--- discover_client.py
+++ discover_client.py
@@ -1,12 +1,13 @@
 import socket
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.settimeout(10)
+s.bind(('0.0.0.0', 0))
+
 def findServer(msg=b'hi'):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.settimeout(10)
-    s.bind(('0.0.0.0', 0))
     s.sendto(msg, ('255.255.255.255', 5000))
 
     reply = s.recvfrom(100)
