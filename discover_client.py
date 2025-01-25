6cdbf07b5778fea3a3ef38a87f0712d89f868fea
Christian Folkers <cfolkers@orontesgames.com>
2025-01-25 01:09:50 +0100

    fix discover client

--- discover_client.py
+++ discover_client.py
@@ -3,6 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('0.0.0.0', 0))
 s.sendto(b'hi', ('255.255.255.255', 5000))
 print(s.recvfrom(100))
 
